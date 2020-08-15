83dc13d55f89134d3c09d2e28e6b5c07f9062d5b
brianbad <brianbad@buffalo.edu>
2020-08-15 15:25:53 -0400

    Updated 1-1

--- src/problem_1_1/Problem1_1.java
+++ src/problem_1_1/Problem1_1.java
@@ -8,23 +8,21 @@ public class Problem1_1 {
 	 * Problem 1.1
 	 * Implement an algorithm to determine if a string has all unique characters. 
 	 * 
+	 * What if you cannot use additional data structures?
+	 * 
+	 * @char[] s - String received as array of characters. Not null.
+	 * 
 	 */
-	public static boolean hasUniqueCharacters(String s) {
+	public static boolean hasUniqueCharacters(char[] s) {
 		// Sort the characters in the string
-		char[] charArray = s.toCharArray();
-		Arrays.sort(charArray);
+		Arrays.sort(s);
 		
 		// If any adjacent characters are the same, return false;
-		for (int i = 1; i < charArray.length; i++) {
-			if (charArray[i] == charArray[i-1]) {
+		for (int i = 1; i < s.length; i++) {
+			if (s[i] == s[i-1]) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
-	/*
-	 * What if you
-	 * cannot use additional data structures?
-	 */
 }
--- src/problem_1_1/Problem1_1Test.java
+++ src/problem_1_1/Problem1_1Test.java
@@ -8,31 +8,31 @@ public class Problem1_1Test {
 
 	@Test
 	public void test1() {
-		boolean result = Problem1_1.hasUniqueCharacters("unique");
+		boolean result = Problem1_1.hasUniqueCharacters("unique".toCharArray());
 		assertEquals(false, result);
 	}
 	
 	@Test
 	public void test2() {
-		boolean result = Problem1_1.hasUniqueCharacters("brian");
+		boolean result = Problem1_1.hasUniqueCharacters("brian".toCharArray());
 		assertEquals(true, result);
 	}
 	
 	@Test
 	public void test3() {
-		boolean result = Problem1_1.hasUniqueCharacters("a bigger test");
+		boolean result = Problem1_1.hasUniqueCharacters("a bigger test".toCharArray());
 		assertEquals(false, result);
 	}
 	
 	@Test
 	public void test4() {
-		boolean result = Problem1_1.hasUniqueCharacters("%$^*()_!");
+		boolean result = Problem1_1.hasUniqueCharacters("%$^*()_!".toCharArray());
 		assertEquals(true, result);
 	}
 	
 	@Test
 	public void test5() {
-		boolean result = Problem1_1.hasUniqueCharacters("");
+		boolean result = Problem1_1.hasUniqueCharacters("".toCharArray());
 		assertEquals(true, result);
 	}
 
